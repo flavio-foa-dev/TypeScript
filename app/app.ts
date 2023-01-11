0d867b7f6a9461e644c6d7df0d9f587598b71924
Flavio Andrade <foa12381@gmail.com>
2023-01-11 10:51:48 -0300

    strictNullChecks

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,16 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
 
 
-const transaction = new NegociacaoController()
-const form: Element = document.querySelector('.form')
+const transaction = new NegociacaoController();
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', (event: Event) => {
-  event.preventDefault();
-  transaction.add()
-})
+if (form) {
+  form.addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+    transaction.add()
+  });
+} else {
+  throw new Error("Nao foi posssil encontar .form ao inicializar aplicacao")
+}
+
 
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,6 +1,12 @@
 export class View {
     constructor(selector) {
-        this.element = document.querySelector(selector);
+        const resultSelector = document.querySelector(selector);
+        if (resultSelector) {
+            this.element = resultSelector;
+        }
+        else {
+            throw new Error(`selector ${selector} not found`);
+        }
     }
     update(model) {
         const template = this.template(model);
